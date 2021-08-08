1dd2e71a2872427b9af26d104c948630d617e9e9
Hashem <hashemabualteen@gmail.com>
2021-08-08 12:15:16 +0300

    Add more logging and documentation

--- src/main/java/com/example/demo/user/UserControllerAdmin.java
+++ src/main/java/com/example/demo/user/UserControllerAdmin.java
@@ -1,33 +1,77 @@
 package com.example.demo.user;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controller for admin access
+ */
 @RestController
 public class UserControllerAdmin {
+
+
+    Logger logger = LoggerFactory.getLogger(UserControllerAdmin.class);
 
     @Autowired
     private UserService userService;
 
+    /**
+     * Welcome string.
+     *
+     * @return the string
+     */
     @GetMapping("/admin/hi")
     public String welcome(){
         return "Welcome admin";
     }
 
+    /**
+     * Create user in a company.
+     *
+     * @param userDto   the user dto
+     * @param companyId the company id
+     * @return the user
+     */
     @PostMapping("/admin/companies/{companyId}/users")
-    public User newUser(@RequestBody UserDto userDto, @PathVariable int companyId) {
+    public User createUserInACompany(@RequestBody UserDto userDto, @PathVariable int companyId) {
+        logger.info("createUserInACompany Method is accessed ");
         User user = new User();
+        if(userDto.getAge() == 0)
+        {
+            logger.warn("The new user age is 0");
+        }
+        if(userDto.getFirstName() == null)
+            logger.warn("The new user first name is null");
+        if(userDto.getLastName() == null )
+            logger.warn("The new user last name is null");
+
         user.setAge(userDto.getAge());
-        user.setCompany(userDto.getCompany());
         user.setFirstName(userDto.getFirstName());
         user.setLastName(userDto.getLastName());
         user.setId(userDto.getId());
         return userService.createUserInACompany(user, companyId);
     }
 
+    /**
+     * Create user.
+     *
+     * @param userDto the user dto
+     * @return the user
+     */
     @PostMapping("/admin/users")
-    public User newUser(@RequestBody UserDto userDto){
+    public User createUser(@RequestBody UserDto userDto){
+        logger.info("createUser method has been accessed ");
+        if(userDto.getAge() == 0)
+        {
+            logger.warn("The new user age is 0");
+        }
+        if(userDto.getFirstName() == null)
+            logger.warn("The new user first name is null");
+        if(userDto.getLastName() == null )
+            logger.warn("The new user last name is null");
         User user = new User();
         user.setAge(userDto.getAge());
         user.setCompany(userDto.getCompany());
@@ -37,8 +81,15 @@ public class UserControllerAdmin {
         return userService.createUser(user);
     }
 
+    /**
+     * Update user user.
+     *
+     * @param userDto the user dto
+     * @return the user
+     */
     @PutMapping("/admin/users")
     public User updateUser(@RequestBody UserDto userDto) {
+        logger.info("updateUser method has been accessed ");
         User user  = new User();
         user.setAge(userDto.getAge());
         user.setCompany(userDto.getCompany());
@@ -48,8 +99,15 @@ public class UserControllerAdmin {
         return userService.updateUser(user);
 
     }
+
+    /**
+     * Delete user.
+     *
+     * @param id the id
+     */
     @DeleteMapping("/admin/users/{id}")
     public void deleteUser(@PathVariable int id) {
+        logger.info("deleteUser method has been accessed ");
         userService.deleteUser(id);
     }
 }
--- src/main/java/com/example/demo/user/UserService.java
+++ src/main/java/com/example/demo/user/UserService.java
@@ -1,13 +1,21 @@
 package com.example.demo.user;
 
 import com.example.demo.company.Company;
+import com.example.demo.company.CompanyNotFoundException;
+import com.example.demo.company.CompanyRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
 @Service
 public class UserService {
+
+    Logger logger = LoggerFactory.getLogger(UserService.class);
+
 
     @Autowired
     private final UserRepository repository;
@@ -15,19 +23,48 @@ public class UserService {
     @Autowired
     private final UserDao userDao;
 
-    public UserService(UserRepository repository, UserDao userDao) {
+    @Autowired
+    private final CompanyRepository companyRepository;
+
+    /**
+     * Instantiates a new User service.
+     *  @param repository the repository
+     * @param userDao    the user dao
+     * @param companyRepository repository for company object
+     */
+    public UserService(UserRepository repository, UserDao userDao, CompanyRepository companyRepository) {
         this.repository = repository;
         this.userDao = userDao;
+        this.companyRepository = companyRepository;
     }
 
 
-
+    /**
+     * Create user in a company .
+     *
+     * @param newUser   the new user
+     * @param companyId the company id
+     * @return the user
+     */
     public User createUserInACompany(User newUser, int companyId) {
+        logger.info("createUserInACompany Method has been accessed");
+        if(!companyRepository.existsById(companyId))
+            throw new CompanyNotFoundException(companyId);
+
         newUser.setCompany(new Company(companyId));
         return repository.save(newUser);
     }
 
+    /**
+     * Update user user.
+     *
+     * @throws UserNotFoundException throws it if user does not exist
+     *
+     * @param updatedUser the updated user
+     * @return the user
+     */
     public User updateUser(User updatedUser) {
+        logger.info("updateUser Method has been accessed");
         User user =  repository.findById(updatedUser.getId())
                 .orElseThrow(() -> new UserNotFoundException(updatedUser.getId()));
 
@@ -41,36 +78,89 @@ public class UserService {
         return repository.save(user);
     }
 
+    /**
+     * Delete user.
+     *
+     * @throws UserNotFoundException if user does not exist
+     *
+     * @param id the id
+     */
     public void deleteUser(int id) {
+        logger.info("deleteUser Method has been accessed");
         if(repository.existsById(id))
             repository.deleteById(id);
         else
             throw new UserNotFoundException(id);
     }
 
+    /**
+     * Search users list.
+     *
+     * @param id        the id
+     * @param firstName the first name
+     * @param lastName  the last name
+     * @param age       the age
+     * @return the list
+     */
     public List<User> searchUsers(String id, String firstName, String lastName, String age) {
-
+        logger.info("SearchUsers Method has been accessed");
         return userDao.findUserByIdAndFirstNameAndLastNameAndAgeAndCompanyId(id, firstName , lastName, age);
 
     }
 
+    /**
+     * Gets by id.
+     *
+     * @throws UserNotFoundException if user does not exist
+     *
+     * @param id the id
+     * @return the User
+     */
     public User getById(int id) {
+        logger.info("getById Method has been accessed");
         return repository.findById(id)
                 .orElseThrow( () -> new UserNotFoundException(id) );
 
     }
 
+    /**
+     * Create user.
+     *
+     * @param newUser the new user
+     * @return the user
+     */
     public User createUser(User newUser) {
+        logger.info("createUser Method has been accessed");
         return repository.save(newUser);
     }
 
+    /**
+     * Gets company users.
+     *
+     * @param companyId the company id
+     * @return the company users
+     */
     public List<User> getCompanyUsers(int companyId) {
+        logger.info("getCompanyUsers Method has been accessed");
         return repository.findByCompanyId(companyId);
     }
 
 
+    /**
+     * General search list.
+     *
+     * @param id              the id
+     * @param firstName       the first name
+     * @param lastName        the last name
+     * @param age             the age
+     * @param companyId       the company id
+     * @param companyName     the company name
+     * @param companyLocation the company location
+     * @return the list
+     */
     public List<User> generalSearch(String id, String firstName, String lastName, String age,
                                     String companyId, String companyName, String companyLocation) {
+        logger.info("GeneralSearch Method has been accessed");
         return userDao.generalUserSearch(id, firstName, lastName, age, companyId, companyName, companyLocation);
     }
 }
